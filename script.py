e4e5560019fd4a16074a565f9971a5769318692b
Andrea Wen-Yi Wang <ayw255@nyu.edu>
2024-04-10 19:19:48 -0400

    add files

--- script.py
+++ script.py
@@ -8,7 +8,8 @@ from torch.nn import CrossEntropyLoss
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--output-file", required=True, type=str)
+parser.add_argument("--output-file", type=str, default='tmp.log')
+parser.add_argument("--cache-size", type=int, default=256)
 parser.add_argument("--window", action='store_true')
 parser.add_argument("--window-with-start", action='store_true')
 args = parser.parse_args()
@@ -34,7 +35,7 @@ with open("10146.txt", "r") as f: # first document of PG19 test set
 encodings = tokenizer(text, return_tensors="pt")
 corpus_seq_len = encodings.input_ids.size(1)
 
-window = 256
+window = args.cache_size
 
 # ---- window attention ----
 if args.window:
@@ -67,14 +68,15 @@ if args.window:
             else:
                 past_key_values = outputs.past_key_values
 
-            print(past_key_values[0][0].shape)
-
         nlls.append(nll)
     f.close()
     ppl = torch.exp(torch.stack(nlls).mean())
     print(ppl)
 ## ---- 4 starting tokens + window attention ----
 if args.window_with_start:
+    from modify_llama_new import enable_llama_pos_shift_attention
+    enable_llama_pos_shift_attention(model)
+    
     f = open(args.output_file, "w")
     n_start = 4
     past_key_values = None
@@ -92,7 +94,7 @@ if args.window_with_start:
             
             logits = outputs.logits.reshape(-1, model.config.vocab_size)
             nll = loss_function(logits.detach().cpu(), labels.view(-1))
-            print(f"nll: {nll.item():.2f}, ppl: {torch.exp(nll).item():.2f}", file=f, flush=True)
+            print(f"loc: {loc}, nll: {nll.item():.2f}, ppl: {torch.exp(nll).item():.2f}", file=f, flush=True)
 
             past_seq_len = outputs.past_key_values[0][0].shape[2]
             if past_seq_len <= window-1:
