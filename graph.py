9748c1d0df0cb2a5c31f28b4b80824032da4e542
Andrea Wen-Yi Wang <ayw255@nyu.edu>
2024-04-10 22:14:04 -0400

    change figure subtitle

--- graph.py
+++ graph.py
@@ -27,6 +27,7 @@ data = pd.DataFrame(data)
 data['rolling_nll'] = data['nll'].rolling(10).mean()
 g = sns.relplot(data = data[data['loc']<=3000], x='loc', y='rolling_nll', col='experiment', kind='line', height=3, aspect=3)
 g.set_ylabels("log(ppl)")
+g.set_titles(col_template="{col_name}")
 
 plt.savefig("figure.pdf")
 
